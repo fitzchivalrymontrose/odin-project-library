85eb085f7bca4a48ad601754f964e3c1ba0c5594
fitzchivalrymontrose <fitzchivalrymontrose@protonmail.com>
2022-01-09 15:22:55 -0500

    Work on addBookToLibrary().

--- library.js
+++ library.js
@@ -19,19 +19,30 @@ Book.prototype.toggleFinishedReading = function (){
     }
 }
 
-const book1 = new Book("Assassin's Fate", "Hobb, Robin");
-book1.toggleFinishedReading();
-console.log(book1);
+const shelf = document.querySelector("#table-data");
 
+const book1 = new Book("Assassin's Fate", "Hobb, Robin",);
+addBookToLibrary(book1);
+const book2 = new Book("Golden Fool", "Hobb, Robin");
+addBookToLibrary(book2);
+displayLib();
 
 function addBookToLibrary(book){
     myLib.push(book);
 }
 
+function removeBookFromLibrary(book) {
+    
+}
+
+
 // Write a function that loops through the array and displays each book on the page. You can display them in some sort of table, or each on their own “card”. It might help for now to manually add a few books to your array so you can see the display
 
 function displayLib(){
     myLib.forEach(book => {
-        //display book        
+        addToShelf(book);       
     });
+}
+function addToShelf(book) {
+    shelf.innerHTML += `<tr><td>${book.title}</td><td>${book.author}</td><td>${book.pages}</td><td>${book.pubYear}</td></tr>`;
 }
