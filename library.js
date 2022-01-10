08ecc013fb7cfbd107b85546b0b3f4db270149de
fitzchivalrymontrose <fitzchivalrymontrose@protonmail.com>
2022-01-09 22:53:27 -0500

    Restructure. Now uses classes instead of prototypes for book instances.

--- library.js
+++ library.js
@@ -1,48 +1,66 @@
 'use strict';
 
 let myLib = [];
-
-function Book(title, author, pages, pubYear){
-    //constructor
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.pubYear = pubYear;
-    this.finishedReading = false;
-}
-Book.prototype.toggleFinishedReading = function (){
-    if(this.finishedReading === true){
-        this.finishedReading = false;
-    }
-    else{
-        this.finishedReading = true;
-    }
-}
-
 const shelf = document.querySelector("#table-data");
 
-const book1 = new Book("Assassin's Fate", "Hobb, Robin",);
-addBookToLibrary(book1);
-const book2 = new Book("Golden Fool", "Hobb, Robin");
-addBookToLibrary(book2);
-displayLib();
+class Book{
+    constructor(title, author, pages, year){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.year = year;
+        this.finishedReading = false;
+        this.id = '';
+    }
+
+    toggleFinishedReading(){
+        if(this.finishedReading === true){
+            this.finishedReading = false;
+        }
+        else this.finishedReading = true;
+    }
+    
+    tellAll(){
+        return [this.title, this.author, this.pages, this.year, this.finishedReading];
+    }
+}
 
 function addBookToLibrary(book){
     myLib.push(book);
+    book.id = `${myLib.indexOf(book)}`;
 }
 
 function removeBookFromLibrary(book) {
     
 }
 
-
-// Write a function that loops through the array and displays each book on the page. You can display them in some sort of table, or each on their own “card”. It might help for now to manually add a few books to your array so you can see the display
-
 function displayLib(){
     myLib.forEach(book => {
-        addToShelf(book);       
+        addBookToDisplay(book);       
     });
 }
-function addToShelf(book) {
-    shelf.innerHTML += `<tr><td>${book.title}</td><td>${book.author}</td><td>${book.pages}</td><td>${book.pubYear}</td></tr>`;
+
+function addBookToDisplay(book) {
+    const bookRow = document.createElement('tr');
+    bookRow.setAttribute('idNum', book.id);
+    book.tellAll().forEach(elem => {
+        let info = document.createElement('td');
+        info.textContent = elem;
+        bookRow.appendChild(info);
+    });
+    shelf.appendChild(bookRow);
 }
+
+
+
+
+
+
+
+const book1 = new Book("Assassin's Fate", "Hobb, Robin",);
+addBookToLibrary(book1);
+const book2 = new Book("Golden Fool", "Hobb, Robin");
+book2.year = 1992;
+addBookToLibrary(book2);
+displayLib();
+
